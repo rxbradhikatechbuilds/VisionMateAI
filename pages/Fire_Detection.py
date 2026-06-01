1773ee24eba18d1203d7a00fb54e7ef7c6c1c6e7
Radhika Bhardwaj <bhardwajradhika885@gmail.com>
2026-06-01 12:42:58 +0530

    Update Fire_Detection.py file

--- pages/Fire_Detection.py
+++ pages/Fire_Detection.py
@@ -552,7 +552,7 @@ st.markdown("""
 # ============================================
 # CONFIGURATION
 # ============================================
-API_URL = "http://localhost:8000"
+API_URL = "%https://visionmateai-1.onrender.com\docs"
 PREDICT_ENDPOINT = f"{API_URL}/predict"
 HEALTH_ENDPOINT = f"{API_URL}/health"
 THRESHOLDS_ENDPOINT = f"{API_URL}/thresholds"
